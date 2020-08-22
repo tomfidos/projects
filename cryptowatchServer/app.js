9c019d6677918e39f6ba6821b6b03220ef9abdb0
Tomasz <fidostomasz@gmail.com>
2020-08-22 16:20:54 +0200

    CryptowatchServer: correcting query string handling in index html.

--- cryptowatchServer/app.js
+++ cryptowatchServer/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const Router = require('./routes');
+const router = require('./routes');
 
 const port = 3000;
 
@@ -8,5 +8,5 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, './public')));
-app.use('/sampleResult', Router);
+app.use('/results', router);
 app.listen(port, () => {console.log(`Server is listening on port ${port}`)});
